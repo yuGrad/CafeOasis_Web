c7d018a1451a939c031658f76d52cbed124fb855
iamdudumon <kksy4511@naver.com>
2024-02-29 16:54:17 +0900

    feat: 카페 검색 기능 추가, error handling -> error page로 렌더링

---
+++ repositorie/Cafe.js
@@ -0,0 +1,13 @@
+const db = require("../db/mongo_db");
+
+const Cafe = {
+    collection: "cafes",
+
+    async getCafesbyNameOrAddr(target){
+        const queryJson = { $or: [{ "cafe_name": { "$regex": target, "$options": "i" } }, { "address": { "$regex": target, "$options": "i" } }] };
+        const cafes = await db.query(this.collection, "find", queryJson);
+        return (cafes);
+    }
+}
+
+module.exports = Cafe;  
--- router/cafe.js
+++ router/cafe.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const Cafe = require("../repositorie/Cafe")
 
-router.get("", (req, res) => {
-  res.render("cafe-main", { login: req.session.login, cafes: [] });
-});
-
-router.get("/search", (req, res) => {
+router.get("", async (req, res) => {
   const target = req.query.target;
-  
-  res.redirect("/cafes");
+  let cafes = [];
+
+  if(target != undefined && target != null && target != ""){
+    try{
+      cafes = await Cafe.getCafesbyNameOrAddr(target);
+    } catch(err){
+      res.render("error", {error: {message: "잘 못 된 접근입니다."}});
+    }
+  }
+  res.render("cafe-main", {login: req.session.login, cafes: cafes});
 });
 
 module.exports = router;
