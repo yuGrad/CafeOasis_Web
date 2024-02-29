b79df29e5f7a3a53733643513cd70a3b6615f85b
iamdudumon <kksy4511@naver.com>
2024-02-29 14:37:48 +0900

    refactor: rename service -> repositorie - 2

--- router/user.js
+++ router/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Employee = require("../service/Employee");
+const Employee = require("../repositorie/Employee");
 
 router.get("/login", (req, res) => {
   res.render("login", { errorMessage: null });
