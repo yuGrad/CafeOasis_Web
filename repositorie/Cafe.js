eb93009ad885ff2488909374c8165abbadcd7e47
iamdudumon <kksy4511@naver.com>
2024-03-18 18:19:21 +0900

    feat: 단일 카페 조회 기능 추가

--- repositorie/Cafe.js
+++ repositorie/Cafe.js
@@ -1,12 +1,20 @@
+const { ObjectId } = require("mongodb");
 const db = require("../db/mongo_db");
 
 const Cafe = {
     collection: "cafes",
 
-    async getCafesbyNameOrAddr(target) {
+    async getCafesByNameOrAddr(target) {
         const queryJson = { $or: [{ "cafe_name": { "$regex": target, "$options": "i" } }, { "address": { "$regex": target, "$options": "i" } }] };
-        const cafes = await db.query(this.collection, "find", queryJson);
+        const projection =  {cafe_name: true, starring: true, phone_number: true, address: true, business_hours: true, image_link: true}
+        const cafes = await db.query(this.collection, "find", queryJson, projection);
         return (cafes);
+    },
+
+    async getCafeById(cafe_id){
+        const queryJson = {_id : new ObjectId(cafe_id)};
+        const cafe = await db.query(this.collection, "findOne", queryJson);
+        return (cafe);
     }
 }
 
--- router/cafe.js
+++ router/cafe.js
@@ -2,18 +2,27 @@ const express = require("express");
 const router = express.Router();
 const Cafe = require("../repositorie/Cafe")
 
+router.get("/:cafe_id", async (req, res) => {
+  const cafe_id = req.params.cafe_id;
+  const cafe = await Cafe.getCafeById(cafe_id);
+  console.log(cafe);
+  res.render("cafe_detail", {login: req.session.login, cafe: cafe});
+})
+
 router.get("", async (req, res) => {
   const target = req.query.target;
   let cafes = [];
 
   if (target != undefined && target != null && target != "") {
     try {
-      cafes = await Cafe.getCafesbyNameOrAddr(target);
+      cafes = await Cafe.getCafesByNameOrAddr(target);
     } catch (err) {
       res.render("error", { error: { message: "잘 못 된 접근입니다." } });
     }
   }
   res.render("cafe-main", { login: req.session.login, cafes: cafes });
 });
+
+
 
 module.exports = router;
