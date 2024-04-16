4949207099584893219fbe45071ac0d6e2d78dc4
iamdudumon <kksy4511@naver.com>
2024-04-17 04:39:57 +0900

    feat: 인증번호 검증 controller 추가

--- router/email.js
+++ router/email.js
@@ -2,24 +2,36 @@ const express = require("express");
 const router = express.Router();
 const emailService = require("../service/emailService");
 
-router.post("/signup", async (req, res) => {
-    const verificationCode = emailService.generateVerificationCode();
-    const to = req.body.email;
-    const subject = "Welcome to Oasis! - Email Code";
-    const html = 
-                `<h2>Hello ${req.body.email}</h2>
+router.post("/signup/request", (req, res) => {
+  const verificationCode = emailService.generateVerificationCode();
+  const to = req.body.email;
+  const subject = "Welcome to Oasis! - Email Code";
+  const html = `<h2>Hello ${req.body.email}</h2>
                 welcome to our service!
                 </br>
                 email code: ${verificationCode}`;
 
-    try{
-        await emailService.sendEmail(to, subject, "test", html);
-    }catch(err){
-        console.error(err);
-        res.render("sign-up", {
-            errorMessage: "이메일 인증 실패",
-        });
-    }
+  try {
+    emailService.sendEmail(to, subject, "test", html);
+    emailService.storeVerificationCode(to, verificationCode, "00:30:00");
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+
+router.post("/signup/verification", async (req, res) => {
+  const { email, user_code } = req.body;
+  try {
+    const result = await emailService.verifyUserCode(email, user_code);
+
+    if (result) res.sendStatus(200);
+    else res.sendStatus(401);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
