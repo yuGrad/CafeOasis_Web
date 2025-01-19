1d425ce6d6e0a6e028f77917a0d7f751c59246c0
iamdudumon <kksy4511@naver.com>
2025-01-19 19:49:44 +0900

    feat: cafe id -> cafe info 캐싱

--- src/service/cafeService.js
+++ src/service/cafeService.js
@@ -1,6 +1,17 @@
 const Cafe = require("../repository/Cafe");
+const CafeCache = require("../repository/CafeCache");
 
 const cafeService = {
+	async fineCafeById(cafeId) {
+		let cafe = await CafeCache.getCafeById(cafeId);
+
+		if (!cafe) {
+			cafe = await Cafe.getCafeById(cafeId);
+			CafeCache.setCafeById(cafeId, cafe);
+		}
+		return cafe;
+	},
+
 	toggleCafeBookmark(cafeId, email, isBookmarked) {
 		if (!isBookmarked) {
 			return Cafe.pushCafeBookmark(cafeId, email);
