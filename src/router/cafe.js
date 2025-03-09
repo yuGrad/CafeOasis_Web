a6f67b0d5f85a2bb30ecd622911976e4668774b1
iamdudumon <kksy4511@naver.com>
2025-03-10 02:53:54 +0900

    docs: /cafes/nearby, Swagger 주석 추가

--- src/router/cafe.js
+++ src/router/cafe.js
@@ -5,7 +5,6 @@ const paramValidationMiddleware = require("../middleware/paramValidationMiddlewa
 const cafeController = require("../controller/cafeController");
 const cafeReviewController = require("../controller/cafeReviewController");
 
-// cafe controller
 router.get("", cafeController.getCafeMain);
 
 router.get(
@@ -14,6 +13,42 @@ router.get(
 	cafeController.getCafesBySearch
 );
 
+/**
+ * @swagger
+ * /cafes/nearby:
+ *   get:
+ *     summary: 주변 카페 찾기
+ *     description: 사용자의 위치와 검색 반경을 기반으로 주변 카페 목록을 조회
+ *     tags: [Cafes]
+ *     parameters:
+ *       - in: query
+ *         name: lat
+ *         required: true
+ *         schema:
+ *           type: number
+ *         description: 사용자의 위도 값
+ *       - in: query
+ *         name: lng
+ *         required: true
+ *         schema:
+ *           type: number
+ *         description: 사용자의 경도 값
+ *       - in: query
+ *         name: radius
+ *         required: true
+ *         schema:
+ *           type: number
+ *           minimum: 1
+ *           maximum: 5
+ *         description: 검색 반경 (1 ~ 5 km)
+ *     responses:
+ *       '200':
+ *         description: 성공적으로 카페 목록을 조회
+ *         content:
+ *           application/json:
+ *       '400':
+ *         description: 잘못된 파라미터 값 (lat, lng, radius 값 확인)
+ */
 router.get("/nearby", cafeController.getCafesByNear);
 
 router.get("/:cafe_id", cafeController.getCafeById);
